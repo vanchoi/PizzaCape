91e3fc6f5f7ae14b58032792458ccd012061dc4c
alekdjurovski <38665312+alekdjurovski@users.noreply.github.com>
2018-05-11 11:54:32 +0200

    payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule
