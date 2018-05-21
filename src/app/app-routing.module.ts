68dc9af07ce15b6d74bf42a70a4696ea1e9aeb0f
Miroslav <miroslav.mitrovski@yahoo.com>
2018-05-21 12:09:45 +0200

    Update for contact and about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { LoginComponent } from './login/login.component';
 import { YoumadeComponent } from './youmade/youmade.component';
 import { OurmenuComponent } from './ourmenu/ourmenu.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent},
   { path: 'youmade', component: YoumadeComponent},
   { path: 'ourmenu', component: OurmenuComponent},
-  { path: 'signup', component: SignUpComponent}
+  { path: 'signup', component: SignUpComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'about', component:AboutComponent}
 ];
 
 @NgModule({
