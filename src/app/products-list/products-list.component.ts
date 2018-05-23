eec459b0b5cdff4d7c544b76cc748ac0262824ab
alekdjurovski <38665312+alekdjurovski@users.noreply.github.com>
2018-05-23 13:34:02 +0200

    ingredients updates

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -8,8 +8,16 @@ import { Ingredient } from '../shared/ingredient.model'
 })
 export class ProductsListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('tomato sauce', 0, 50),
-    new Ingredient('fresh tomato', 0, 10)
+    new Ingredient('Tomato Sauce', 0, 10000 ),
+        new Ingredient('Fresh Tomato', 0, 10000 ),
+        new Ingredient('Fresh Peppers', 0, 10000 ),
+        new Ingredient('Olives', 0, 10000 ),
+        new Ingredient('Mushrooms', 0, 10000 ),
+        new Ingredient('Feta Cheese', 0,10000 ),
+        new Ingredient('Mozzarella', 0, 10000 ),
+        new Ingredient('Smoked Ham', 0, 10000 ),
+        new Ingredient('Bacon', 0, 10000 ),
+        new Ingredient('Oregano', 0, 10000 )
 
   ];
 
