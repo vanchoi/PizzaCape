8a16f7af66a2e71b34843b2243fd93ae6b88cc95
alekdjurovski <38665312+alekdjurovski@users.noreply.github.com>
2018-05-29 13:52:26 +0200

    ingredients size and quantity finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -46,6 +47,7 @@ import { ManagmentIngredientsComponent } from './managment-ingredients/managment
     
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/youmade/youmade.component.ts
+++ src/app/youmade/youmade.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+
+import { IngredientsComponent } from './ingredients/ingredients.component'
 
 
 @Component({
@@ -8,19 +10,32 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./youmade.component.css']
 })
 export class YoumadeComponent implements OnInit {
+  sizeM: number = 10;
+  sizeL: number = 20;
+  sizePrice: number = this.sizeM;
   totalPrice:number = 0;
+  ingredientsPrice:number = 0;
+  quantity: number = 1;
+
   onPriceChange(total){
-    this.totalPrice = total;
+    this.ingredientsPrice = total;
+    this.totalPrice = this.ingredientsPrice + this.sizePrice;
   }
 
   constructor() {
     
-    
-    
     }
 
   ngOnInit() {
+    this.onSizeChange('M');
   }
 
-  
+  onSizeChange(size:string){
+    if(size==='M'){
+      this.sizePrice = this.sizeM;
+    }else if(size==='L'){
+      this.sizePrice = this.sizeL;
+    }
+    this.totalPrice = this.ingredientsPrice + this.sizePrice;
+  }
 }
